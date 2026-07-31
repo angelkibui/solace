0590917033764cc7bd8254a7bc0ac2b240718162
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-07-31 08:37:16 +0200

    Build mobile money checkout experience

--- lib/features/auth/presentation/widgets/auth_gate.dart
+++ lib/features/auth/presentation/widgets/auth_gate.dart
@@ -11,11 +11,16 @@ import '../../../therapists/data/repositories/therapist_repository.dart';
 import '../../../therapists/presentation/bloc/therapist_bloc.dart';
 import '../../../therapists/presentation/bloc/therapist_event.dart';
 import '../../../therapists/presentation/pages/therapist_list_screen.dart';
+import '../../../appointments/data/models/appointment_model.dart';
 import '../../../appointments/data/repositories/appointment_repository.dart';
 import '../../../appointments/presentation/bloc/appointment_bloc.dart';
 import '../../../appointments/presentation/bloc/appointment_event.dart';
 import '../../../appointments/presentation/pages/booking_flow_screen.dart';
 import '../../../therapists/data/models/therapist_model.dart';
+import '../../../payments/data/repositories/payment_repository.dart';
+import '../../../payments/presentation/bloc/payment_bloc.dart';
+import '../../../payments/presentation/bloc/payment_event.dart';
+import '../../../payments/presentation/pages/payment_checkout_screen.dart';
 
 class AuthGate extends StatefulWidget {
   final bool startAtRegister;
@@ -46,6 +51,10 @@ class _AuthGateState extends State<AuthGate> {
                 BlocProvider(
                   create: (_) => AppointmentBloc(AppointmentRepository())
                     ..add(AppointmentsRequested(user.uid)),
+                ),
+                BlocProvider(
+                  create: (_) => PaymentBloc(PaymentRepository())
+                    ..add(TransactionsRequested(user.uid)),
                 ),
               ],
               child: _AuthenticatedExperience(userId: user.uid),
@@ -81,7 +90,33 @@ class _AuthenticatedExperience extends StatelessWidget {
       MaterialPageRoute(
         builder: (_) => BlocProvider.value(
           value: context.read<AppointmentBloc>(),
-          child: BookingFlowScreen(userId: userId, therapist: therapist),
+          child: BookingFlowScreen(
+            userId: userId,
+            therapist: therapist,
+            onAppointmentCreated: (appointment) =>
+                _openCheckout(context, appointment, therapist),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _openCheckout(
+    BuildContext context,
+    AppointmentModel appointment,
+    TherapistModel therapist,
+  ) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (_) => MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: context.read<AppointmentBloc>()),
+            BlocProvider.value(value: context.read<PaymentBloc>()),
+          ],
+          child: PaymentCheckoutScreen(
+            appointment: appointment,
+            therapist: therapist,
+          ),
         ),
       ),
     );
